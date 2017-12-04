dd16aaf15dc11dd53e34a40b345512d5cf22eda4
monofrio <onofri04@Gmail.com>
2017-12-03 21:31:22 -0500

    update json data time format

--- fungi_controller/data.php
+++ fungi_controller/data.php
@@ -1,23 +1,20 @@
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="https://www.markonofrio.com/images/favicon.png">
-    <title>Temp Data Statuses</title>
-  </head>
-  <body>
-    <?php
+<?php
     include 'config.php';
       $db_tableName = 'temp'; //UPDATE TABLE NAME
-      $sql = "SELECT * FROM temp";
+      $sql = 'SELECT * FROM temp';
 
       $result = $dbh->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 
       $return = [];
       foreach ($result as $row) {
+
+//**    SAMPLE DATA: {"date": "2017-12-03 01:26:31", "fahrenheit": "71"}
+//**    DATE FORMAT: 24-Apr-07
+          $time = strtotime($row['date']);
+          $myFormatForView = date('d-M-y', $time);
+
           $return[] = [
-                'date' => $row['date'],
+                'date' => $myFormatForView,
                 'fahrenheit' => $row['fahrenheit']
               ];
       }
@@ -28,5 +25,3 @@
       // Close connection.
       mysqli_close($conn);
  ?>
-</body>
-</html>
