f3acf20c4e89a3f3c1196d48b702a76c89c8ba10
monofrio <onofri04@Gmail.com>
2017-12-08 00:46:14 -0500

    updated

--- fungi_controller/index.php
+++ fungi_controller/index.php
@@ -6,9 +6,79 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="https://www.markonofrio.com/images/favicon.png">
     <title>Temp Statuses</title>
+
     <link href='style.css' rel="stylesheet" type="text/css" />
+    <script src="https://d3js.org/d3.v4.min.js"></script>
+
   </head>
   <body>
+  <script>
+      // Sample Data: {"date":"2017-12-03 01:26:31","fahrenheit":"71"}
+      // Formated Data:  { "date": "03-Dec-17", "fahrenheit": "71"}
+      d3.select("body").append("h1").text("Temp Chart");
+      d3.select("body").append("div").attr("id", "stats");
+      d3.select("body").append('svg').attr("width", "500").attr("height", "300");
+
+      var svg = d3.select("svg"),
+          margin = {top: 20, right: 20, bottom: 30, left: 50},
+          width = +svg.attr("width") - margin.left - margin.right,
+          height = +svg.attr("height") - margin.top - margin.bottom,
+          g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      var x = d3.scaleTime()
+          .rangeRound([0, width]);
+
+      var y = d3.scaleLinear()
+          .rangeRound([height, 0]);
+
+
+      d3.json('http://esp8266.markonofrio.com/fungi_controller/data.php',
+          function(error, d) {
+              if (error) return console.warn(error);
+
+              var rows = d.length;
+              console.log("Number of records: ",rows);
+
+              var sumOfTemp = [];
+              var numberOfRecords = 360;
+
+
+              for(var i = 0; i < numberOfRecords; i++){
+                  sumOfTemp.push(d[i].fahrenheit);
+              }
+              var tempSum = d3.sum(sumOfTemp),
+                  averageTemp = (tempSum / numberOfRecords).toFixed(2),
+                  maxTemp = d3.max(sumOfTemp),
+                  minTemp = d3.min(sumOfTemp);
+              // Fahrenheit Data Log
+              console.log("Sum of last 30 Temps: ", tempSum);
+              console.log("Average Temp: ", averageTemp);
+              console.log("Max Temp: ", maxTemp);
+              console.log("Min Temp: ", minTemp);
+
+              var stats = d3.select("div");
+              stats.append("table");
+              stats.append("th").text("Fahrenheit Data Log for the last " + (numberOfRecords * 10 / 60 ) + " minutes");
+
+              stats.append("tr").text(d[0].date);
+
+              stats.append("tr");
+              stats.append("td").text("Max Temp: ");
+              stats.append("td").text(maxTemp);
+
+              stats.append("tr");
+              stats.append("td").text("Min Temp: ");
+              stats.append("td").text(minTemp);
+
+              stats.append("tr");
+              stats.append("td").text("Avarage Temp: ");
+              stats.append("td").text(averageTemp);
+
+
+
+
+          });
+  </script>
   <div style="display:block; float:left;"></div>
 <?php
     include 'config.php';
@@ -36,4 +106,5 @@
       // Close connection.
       mysqli_close($conn);
     ?>
+
 </html>
